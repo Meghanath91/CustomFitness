1c7dfacbf8c497f18b37c33e3645c65bd8ce7569
Meghanath91 <meghanath.balaji@gmail.com>
2020-03-14 15:42:37 -0400

    sidebar progress

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,26 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Dashboard/Sidebar";
+import Card from "./components/Card/Card";
 
 function App() {
+  // Store global state like user here
+
+  // handleLong
+
+  
   return (
     <div>
-      <Navbar />
+      {/* Router */}
+        {/* always showing  - outside of switch */}
+        <Navbar /> 
+        <Sidebar/>
+
+        {/* Switch */}
+          {/* Route = .... */}
+          <Card/>
+          <dashboard_trainer/>
     </div>
   );
 }
