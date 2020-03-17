032dbad343a691e6e2b3ef5368a6db6583c8c67f
parthjd <parthjd28@gmail.com>
2020-03-17 13:35:24 -0400

    added drag and drop

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,12 +4,12 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
 import ExerciseList from "./components/Customplan/ExerciseList";
+import ExerciseListItem from "./components/Customplan/ExerciseListItem";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <ExerciseList />
     </div>
   );
 }
--- my-app/src/components/Customplan/ExerciseList.js
+++ my-app/src/components/Customplan/ExerciseList.js
@@ -1,36 +1,41 @@
 import React from "react";
-import ExerciseListItem from "./ExerciseListItem";
 import { makeStyles } from "@material-ui/core/styles";
+
 const useStyles = makeStyles(theme => ({
-  root: {
+  board: {
     display: "flex",
-    justifyContent: "flex-end",
-    margin: "0% auto"
+    flexDirection: "column",
+    width: "25%",
+    height: "100vh",
+    backgroundColor: "#2B1B00"
   }
 }));
 
-export default function ExerciseList() {
+export default function ExerciseList(props) {
   const classes = useStyles();
+  const drop = e => {
+    e.preventDefault();
+    const card_id = e.dataTransfer.getData("card_id");
+
+    const card = document.getElementById(card_id);
+    card.style.display = "block";
+
+    e.target.appendChild(card);
+  };
+
+  const dragOver = e => {
+    e.preventDefault();
+  };
+
   return (
-    <div className={classes.root}>
-      <ExerciseListItem
-        image_url="https://thumbs.gfycat.com/GlossySkinnyDuckbillcat-small.gif"
-        title="Push-up"
-        name="Push-ups"
-        description="push-up is an exercise done laying with face, palms and toes facing down, keeping legs and back straight, extending arms straight to push body up and back down again."
-      />
-      <ExerciseListItem
-        image_url="https://i.imgur.com/UJAnRhJ.gif?noredirect"
-        title="Sit-up"
-        name="Sit-ups"
-        description="a type of physical exercise for making stomach muscles strong, in which you lie on your back on the floor and rouse the top part your body to a sitting position"
-      />
-      <ExerciseListItem
-        image_url="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/workouts/2016/03/jumpingjack-1457045563.gif"
-        title="Jumping Jack"
-        name="Jumping Jack"
-        description="a physical jumping exercise performed by jumping to a position with the legs spread wide and the hands touching overhead, sometimes in a clap, and then returning to a position with the feet together and the arms at the sides"
-      />
+    <div
+      className={classes.board}
+      id={props.id}
+      onDrop={drop}
+      onDragOver={dragOver}
+      name={props.value}
+    >
+      {props.children}
     </div>
   );
 }
