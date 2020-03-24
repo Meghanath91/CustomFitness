afdb6937663f29c0c101bc2c4194f5532a0e2995
Meghanath91 <meghanath.balaji@gmail.com>
2020-03-24 17:05:46 -0400

    customplan fixes

--- my-app/src/components/Trainer/Customplan/Customplan.js
+++ my-app/src/components/Trainer/Customplan/Customplan.js
@@ -4,13 +4,26 @@ import "./CustomPlan.scss";
 
 import CustomForm from "./CustomForm";
 
+const [title,setTitle]= useState("");
+const [description,setDescription]= useState("");
+const [difficulty,setDifficulty]= useState("");
+const [type,setType]=useState("");
+const[sets,setSets]=useState("");
+const[reps,setReps]=useState("");
 
 export default function CustomPlan(props) {
   
   
   return (
     <section className="custom-plan">
-      <CustomForm className="custom-form" />
+      <CustomForm className="custom-form"
+        setTitle={setTitle}
+        setDescription={setDescription}
+        setDifficulty={setDifficulty}
+        setType={setType}
+        setSets={setSets}
+        setReps={setReps}
+      />
       <ExerciseList exerciseData={props.exerciseData}/>
     </section>
   );
