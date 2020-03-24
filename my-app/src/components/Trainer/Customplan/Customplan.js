616bdff6ae044aab8ec1ff594ed9b8368dd28631
Meghanath91 <meghanath.balaji@gmail.com>
2020-03-24 18:18:41 -0400

    custom plan table populated

--- my-app/src/components/Trainer/Customplan/Customplan.js
+++ my-app/src/components/Trainer/Customplan/Customplan.js
@@ -1,17 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import ExerciseList from "./ExerciseList";
 import "./CustomPlan.scss";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
 
 import CustomForm from "./CustomForm";
 
-
 export default function CustomPlan(props) {
-  
-  
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [type, setType] = useState("");
+  const [sets, setSets] = useState("");
+  const [reps, setReps] = useState("");
+
+  const handleCreatePlan = evt => {
+    evt.preventDefault();
+
+    axios.post(`http://localhost:8080/custom_plans/create`, {
+        trainer_id: 1,
+        student_id: 1,
+        title: title,
+        description: description,
+        difficulty: difficulty,
+        type: type
+      })
+      .then(res => {
+        console.log("i get to this point with user from======>>>", res.data);
+        return <Redirect to="/trainer" />;
+      });
+  };
+
   return (
     <section className="custom-plan">
-      <CustomForm className="custom-form" />
-      <ExerciseList exerciseData={props.exerciseData}/>
+      <CustomForm
+        className="custom-form"
+        setTitle={setTitle}
+        setDescription={setDescription}
+        setDifficulty={setDifficulty}
+        setType={setType}
+        setSets={setSets}
+        setReps={setReps}
+        handleCreatePlan={handleCreatePlan}
+      />
+      <ExerciseList exerciseData={props.exerciseData} />
     </section>
   );
-};
+}
