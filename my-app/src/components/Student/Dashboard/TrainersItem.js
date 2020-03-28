c472583a48f040841a552cd4c0384abae9b7b451
Meghanath91 <meghanath.balaji@gmail.com>
2020-03-28 19:28:14 -0400

    code cleaned up

--- my-app/src/components/Student/Dashboard/TrainersItem.js
+++ my-app/src/components/Student/Dashboard/TrainersItem.js
@@ -1,26 +1,22 @@
 import React, { useState, useEffect } from "react";
-import AppBar from "@material-ui/core/AppBar";
+
 import Button from "@material-ui/core/Button";
-import CameraIcon from "@material-ui/icons/PhotoCamera";
+
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
-import Toolbar from "@material-ui/core/Toolbar";
+
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Link from "@material-ui/core/Link";
 import axios from "axios";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
+
 import { useTheme } from "@material-ui/core/styles";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
+
 import TrainerView from "./TrainerView"
 
 
@@ -90,17 +86,10 @@ export default function StudentsItem(props) {
       });
   };
 
-  const [open, setOpen] = React.useState(false);
   const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+ 
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
+ 
 
   return (
     <React.Fragment>
