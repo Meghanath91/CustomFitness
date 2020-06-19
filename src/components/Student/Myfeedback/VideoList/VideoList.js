3b672021d1deceaee6161eb8e1298fb778d78f72
Meghanath91 <meghanath.balaji@gmail.com>
2020-06-19 01:49:17 -0400

    no video available added

--- src/components/Student/Myfeedback/VideoList/VideoList.js
+++ src/components/Student/Myfeedback/VideoList/VideoList.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
 import axios from "axios";
-
+// import logo from "pictures/no_video.png"
 const VideoList = (props) => {
   const [feedback, setFeedback] = useState([]);
   useEffect(() => {
     axios.get(`/student/${props.student.id}/feedbacks`).then((res) => {
       const studentFeedbackData = res.data;
-      console.log("data=>", res.data);
+      console.log(res.data,"11")
       setFeedback(studentFeedbackData);
     });
   }, [props.student.id]);
@@ -16,15 +16,20 @@ const VideoList = (props) => {
       <h1>My feedbacks</h1>
       <div className="video-container">
         {feedback.map((feedback) => {
-          return(
-          <div>
-          <p>Message: {feedback.feedback_text}</p>
-          <ReactPlayer
-            key={feedback.id}
-            className="video"
-            url={feedback.feedback_video}
-          />
-          </div>);
+          return (
+            <div>
+              <p>Message: {feedback.feedback_text}</p>
+              {feedback.feedback_video ? (
+                <ReactPlayer
+                  key={feedback.id}
+                  className="video"
+                  url={feedback.feedback_video}
+                />
+              ) : (
+                <img src="/pictures/no_video.png" alt="no video available"/>
+              )}
+            </div>
+          );
         })}
       </div>
     </div>
