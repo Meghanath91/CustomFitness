c1a9bd947531e915fce7901fe1cde1a6821e259b
Meghanath91 <meghanath.balaji@gmail.com>
2020-06-29 17:34:01 -0400

    design-fix- feedback items

--- src/components/Trainer/Feedback/Video/VideoContainer.js
+++ src/components/Trainer/Feedback/Video/VideoContainer.js
@@ -11,19 +11,20 @@ export default function VideoContainer(props) {
         {props.feedback[0] //conditional rendering if there is feedbacks map feedbacks
           ? props.feedback.map((feedback) => {
               return (
-                <div>
-                  <h1 className="student-name">{feedback.student_id}</h1>
+                <div className="feedback-box">
                   <p className="student-feedback-text">
-                    <strong>Feedback: </strong> {feedback.feedback_text}
+                    <strong>Message: </strong> {feedback.feedback_text}
                   </p>
                   <ReactPlayer
-                    className="video"
+                    className="feedback-video"
+                    width="450px"
+                    height="200px"
                     url={feedback.feedback_video}
                   />
                 </div>
               );
             })
-          : "no feedbacks"}
+          : <div className="empty-container">no feedbacks</div>}
       </div>
     
   );
